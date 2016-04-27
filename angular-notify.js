06a2af95492bc2b1fa417ac8f9f6eaf95d891b40
Davi Ferreira <contato@daviferreira.com>
2016-04-27 10:52:59 +0200

    Use interval to respect angular cycle and avoid multiple notifications

--- angular-notify.js
+++ angular-notify.js
@@ -32,8 +32,12 @@ angular.module('cgNotify', []).factory('notify',['$interval','$http','$compile',
             if (maximumOpen > 0) {
                 var numToClose = (openNotificationsScope.length + 1) - maximumOpen;
                 for (var i = 0; i < numToClose; i++) {
-                    if (openNotificationsScope[i] && openNotificationsScope[i].hasOwnProperty('$close')) {
-                        openNotificationsScope[i].$close();
+                    if (openNotificationsScope[i]) {
+                        $interval(function(){
+                            if (openNotificationsScope[i].hasOwnProperty('$close')) {
+                                openNotificationsScope[i].$close();
+                            }
+                        }, 0, 1);
                     }
                 }
             }
